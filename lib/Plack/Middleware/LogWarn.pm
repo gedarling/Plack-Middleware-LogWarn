5fbed02bdc9e357a2b611a9f61e4aff339487d71
Geoffrey Darling <geoffreydarling@gmail.com>
2011-05-20 16:41:08 -0500

    added POD

--- lib/Plack/Middleware/LogWarn.pm
+++ lib/Plack/Middleware/LogWarn.pm
@@ -1,5 +1,6 @@
 package Plack::Middleware::LogWarn;
 use strict;
+use warnings;
 use parent qw( Plack::Middleware );
 use Plack::Util::Accessor qw( logger );
 
@@ -18,3 +19,50 @@ sub call {
 }
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+builder {
+   enable 'LogWarn';
+   $app;
+}
+
+# use it with another logger middleware
+
+builder {
+   enable 'LogWarn';
+   enable 'Log4perl', category => 'plack', conf => '/path/to/log4perl.conf';
+   $app;
+}
+
+=head1 DESCRIPTION
+
+LogWarn is a C<Plack::Middleware> component that will help you get warnings into
+a logger. You probably want to use some sort of real logging system such as
+L<Log::Log4perl> and another C<Plack::Middleware> such as L<Plack::Middleware::Log4perl>.
+
+=head1 CONFIGURATION
+
+=over 4
+
+=item logger
+
+optional, C<coderef> that will capture warnings. By default it uses
+C<< $env->{'psgix.logger'} >> with a level of C<warn>.
+
+=back
+
+=head1 SEE ALSO
+
+L<Plack::Middleware::Log4perl>
+
+=head1 CREDITS
+
+Thanks to Micro Technology Services, Inc. for funding the initial development
+of this module and frew (Arthur Axel "fREW" Schmidt <frioux@gmail.com>) for his
+extensive patience and assistance.
+
+
+=cut
